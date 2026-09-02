1764356959fd7c9ffb9172043b9202b78ae0b08c
buildwithanuska <anuskadasxa@gmail.com>
2026-09-02 23:57:42 +0530

    Add 1881A - Don't Try to Count solution

---
+++ 1881A_Dont_Try_to_Count.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <string> 
+ 
+using namespace std; 
+ 
+void solve() { 
+    int n, m; 
+    cin >> n >> m; 
+    string x, s; 
+    cin >> x >> s; 
+     
+    for(int i = 0; i <= 6; ++i) { 
+        if (x.find(s) != string::npos) {  
+            cout << i << "\n"; 
+            return; 
+        } 
+        x += x;  
+    } 
+    cout << -1 << "\n"; 
+} 
+ 
+int main() { 
+    int t; 
+    cin >> t; 
+    while (t--) { 
+        solve(); 
+    } 
+    return 0; 
+}
