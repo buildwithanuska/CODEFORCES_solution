704d10a8a83b223865339969bae150e6d32b301f
buildwithanuska <anuskadasxa@gmail.com>
2026-09-03 20:47:29 +0530

    Add Codeforces 1903A solution

---
+++ 1903A_Sort_the_Subarray.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        if (k >= 2) {
+            cout << "YES\n";
+        }
+        else {
+            bool sort = true;
+
+            for (int i = 1; i < n; i++) {
+                if (a[i] < a[i - 1]) {
+                    sort = false;
+                    break;
+                }
+            }
+
+            if (sort)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
