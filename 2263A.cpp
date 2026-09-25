1956b0ec00ba77387ea146ad37f35c623957e6cc
buildwithanuska <anuskadasxa@gmail.com>
2026-09-25 10:42:54 +0530

    Update solution for Codeforces 2263A - Min Max Game

--- 2263A.cpp
+++ 2263A.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
-
 using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int countOnes = 0;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        if (x == 1) {
+            countOnes++;
+        }
+    }
+
+    int elsieTurns = (n - 1) / 2;
+
+    if (countOnes > elsieTurns) {
+        cout << "Bessie\n";
+    }
+    else {
+        cout << "Elsie\n";
+    }
+}
 
 int main() {
     int t;
     cin >> t;
 
     while (t--) {
-        int n;
-        cin >> n;
-
-        int ones = 0;
-
-        for (int i = 0; i < n; i++) {
-            int x;
-            cin >> x;
-
-            if (x == 1) {
-                ones++;
-            }
-        }
-
-        int zeros = n - ones;
-
-        if (ones >= zeros) {
-            cout << "Bessie" << endl;
-        } else {
-            cout << "Elsie" << endl;
-        }
+        solve();
     }
 
     return 0;
